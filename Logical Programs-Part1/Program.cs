6d412717a5a277362dd052e80f72821ffde983b6
duvvamounika <mounikaduvva05@gmail.com>
2022-04-06 14:28:29 +0530

    Add fibonacciseriesLPP1 branch and program

---
+++ Logical Programs-Part1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs_Part1
+{
+    class FibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            int n, f1=0, f2=1, f3;
+            Console.WriteLine("enter the number of element:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The fibonacci Series is:");
+            Console.WriteLine(f1 + ""+ f2+"");
+            f3 = f1 + f2;
+            while(f3<=n)
+            {
+                Console.WriteLine(f3 + "");
+                f1 = f2;
+                f2 = f3;
+                f3 = f1 + f2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
