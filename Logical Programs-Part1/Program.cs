7b6d903a6a374eedcacdb08b0f97ed2db870ced8
duvvamounika <mounikaduvva05@gmail.com>
2022-04-06 19:50:51 +0530

    Add StopWatchLPP6 branch and program

--- Logical Programs-Part1/Program.cs
+++ Logical Programs-Part1/Program.cs
@@ -3,31 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Logical_Programs_Part1
 {
-    class CouponNumber
+    class StopWatch
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("please enter how many distinct number of coupons needed :");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Random random = new Random();
-            List<int> list = new List<int>();
-            int i = 0, count = 0;
-            while (i < num)
-            {
-                int res = random.Next(1, 101);
-                count++;
-                if (!list.Contains(res))
-                {
-                    list.Add(res);
-                    i++;
-                    Console.WriteLine(res);
-                }
-            }
-            Console.WriteLine("{0} times used random class to get {1} distinct coupon numbers", count, num);
+            DateTime Start = DateTime.Now;
+            DateTime End = DateTime.Now ;
+            Console.WriteLine("Elapsed time:" + (End - Start));
             Console.ReadLine();
+
         }
     }
 }
